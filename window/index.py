7acb6933f0507d22198b9cf02295d3ef8e4b9bc7
Khepri12345 <A123456789@mail.com>
2024-05-28 11:33:09 +0800

    review

--- window/index.py
+++ window/index.py
@@ -1,5 +1,11 @@
-with open('names.txt',encoding="utf-8") as file:
-    content:str = file.read()
-names:list[str] = content.split()
-for name in names:
-    print(name)
+def get_names() -> list[str]: #區域變數
+    with open('names.txt',encoding="utf-8") as file:
+        content:str = file.read()
+    names:list[str] = content.split()
+    # for name in names:
+    # print(name)
+    return names
+# names:list[str] = get_names()
+if __name__ == "__main__": #文件變數
+    names:list[str] = get_names()
+    print(names)
