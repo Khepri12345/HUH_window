57aadf0b792e10bcc9da95fcbf85f4793928dcfa
Khepri12345 <A123456789@mail.com>
2024-05-31 08:40:10 +0800

    review

--- window_layout/index1.py
+++ window_layout/index1.py
@@ -2,15 +2,19 @@ import tkinter as tk
 from tkinter import ttk
 
 class Window(tk.Tk):
-    def __init__(self, **kwargs):
+    def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        self.title("pack")
+        self.title("pack1")
         self.geometry('300x200')
-
-        ttk.Button(self, text="Top").pack()
-        ttk.Button(self, text="Middle").pack()
-        ttk.Button(self, text="Bottom").pack()
         
-if __name__ == 'main':
+        ttk.Button(self,text="Top").pack(side='top', expand=1,fill='both')  
+
+        ttk.Button(self,text="Middle").pack(side='top', expand=1,fill='both')  
+
+        ttk.Button(self,text="Bottom").pack(side='top', expand=1,fill='both')
+        
+
+
+if __name__ == '__main__':
     window:Window = Window()
     window.mainloop()
--- window_layout/index2.py
+++ window_layout/index2.py
@@ -7,10 +7,10 @@ class Window(tk.Tk):
         self.title("pack")
         self.geometry('500x200')
 
-        ttk.Button(self, text="Left").pack(side='left')
-        ttk.Button(self, text="Center Button").pack(side='left')
-        ttk.Button(self, text="Right").pack(side='left')
+        ttk.Button(self, text="Left").pack(side='top', expand=1,fill='x')
+        ttk.Button(self, text="Center Button").pack(side='top', expand=1,fill='x')
+        ttk.Button(self, text="Right").pack(side='top', expand=1,fill='x')
         
-if __name__ == 'main':
+if __name__ == '__main__':
     window:Window = Window()
     window.mainloop()
