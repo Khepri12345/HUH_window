04a793a58b864a818947745e10059f6a5f7bf40f
Khepri12345 <A123456789@mail.com>
2024-07-15 11:12:24 +0800

    review

--- window_0715/main.py
+++ window_0715/main.py
@@ -10,4 +10,9 @@ def index():
     selected_area = request.args.get('area')
     areas = [tup[0] for tup in data.get_areas()]
     selected_area = '士林區'if selected_area is None else selected_area  
-    return render_template('index.html.jinja',areas=areas,show_area=selected_area)
+    detail_snaes = data.get_snaOfArea(area=selected_area)
+
+    #areas->所有行政區 
+    #show_area -> 要顯示的行政區
+    #detail_snaes -> 該行政區所有站點資訊   
+    return render_template('index.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)
