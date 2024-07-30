b5d541e711a0998ad146dbda40c2ea410786dd94
Khepri12345 <A123456789@mail.com>
2024-07-30 11:29:41 +0800

    review

--- window_0730/auth/main.py
+++ window_0730/auth/main.py
@@ -3,5 +3,7 @@ from flask import Blueprint
 auth_blueprint = Blueprint('auth',__name__)
 
 @auth_blueprint.route('/auth/')
+
+@auth_blueprint.route('/auth/login')
 def index():
-    return "<h1>我是首頁</h1>"
+    return "<h1>我是auth首頁</h1>"
--- window_0730/main.py
+++ window_0730/main.py
@@ -32,8 +32,9 @@ def index1():
     #detail_snaes -> 該行政區所有站點資訊   
     return render_template('index1.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)    
 
-    
-
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html.jinja'),404
 
 if __name__ == "__main__":
     run_simple("localhost", 8080, application,use_debugger=True,use_reloader=True)
