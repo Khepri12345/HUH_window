f27b8fa97d8f815823436ab4feaa3bd6688fbf8e
Khepri12345 <A123456789@mail.com>
2024-07-31 09:51:21 +0800

    review

--- window_0731/auth/main.py
+++ window_0731/auth/main.py
@@ -33,3 +33,8 @@ def index():
     else:
         print("第一次進入")
     return render_template('/auth/login.html.jinja',form=form)
+
+@auth_blueprint.route("/auth/logout")
+def logout():
+    session.pop('username')
+    return redirect('/')
